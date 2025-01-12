2509bc021ccc3a421ccc3bab2980bcc6dea1cbc8
danielc <88316439+dandan59832@users.noreply.github.com>
2025-01-12 17:23:33 +0200

    start of vision code

---
+++ src/main/java/frc/robot/subsystems/Vision/VisionConstants.java
@@ -0,0 +1,34 @@
+package frc.robot.subsystems.Vision;
+
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform3d;
+
+public class VisionConstants {
+    public enum CameraConstants{
+        FRONT_LEFT("front left",
+            new Transform3d(
+                0, 0, 0,
+                new Rotation3d(0, 0, 0))),
+
+
+        BACK_MIDDLE("back middle",
+            new Transform3d(
+                0, 0, 0,
+                new Rotation3d(0, 0, 0)));
+
+        public final String cameraName;
+
+        public final Transform3d robotToCamera;
+
+        private CameraConstants(String name, Transform3d robotToCamera){
+            this.cameraName = name;
+            this.robotToCamera = robotToCamera;
+        }
+
+        @Override
+        public String toString(){
+            return cameraName;
+        }
+    }
+ 
+}
