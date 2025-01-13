000446357eade997a6bc45c3e60e25049fd0a1cb
coders-code-well <smasherisaac@gmail.com>
2025-01-13 08:28:48 +0200

    Start code for Elevator (Motor Interface, Subsystem, start of commands) ~ Asher

---
+++ src/main/java/frc/robot/subsystems/Elevator/Elevator.java
@@ -0,0 +1,39 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.Elevator;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.subsystems.Elevator.ElevatorConstants.ElevatorLevel;
+import frc.robot.subsystems.Elevator.ElevatorIO.ElevatorInputs;
+
+public class Elevator extends SubsystemBase {
+  /** Creates a new Elevator. */
+  ElevatorIO io;
+  ElevatorInputs inputs = new ElevatorInputs(); /// TODO: Check about AutoLogged
+  ElevatorLevel level;
+
+  public Elevator() {
+    // io = new IO();
+    level = ElevatorLevel.NULL;
+  }
+
+  public void resetMotorEncoder(){
+    io.resetMotorEncoder();
+    level = ElevatorLevel.Bottom;
+  }
+
+  public void moveMotorByPosition(double position){
+    io.moveMotorByPosition(position);
+  }
+
+  public void setLevelVariable(ElevatorLevel level){
+    this.level = level;
+  }
+
+  @Override
+  public void periodic() {
+    io.Update(inputs);
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/Elevator/ElevatorConstants.java
@@ -0,0 +1,21 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.Elevator;
+
+/** Add your docs here. */
+public class ElevatorConstants {
+    public enum ElevatorLevel{
+        Bottom,
+        L1,
+        L2,
+        L3,
+        L4,
+        Intake,
+        Moving,
+        NULL
+    }
+
+    
+}
