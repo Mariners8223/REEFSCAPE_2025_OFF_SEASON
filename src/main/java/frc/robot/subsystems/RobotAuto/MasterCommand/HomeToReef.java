5fd8730fb0e0933640dce6430f4679854648e158
captainsoccer <justeyallevi@gmail.com>
2025-01-19 11:42:47 +0200

    wrote home to reef

--- src/main/java/frc/robot/subsystems/RobotAuto/MasterCommand/HomeToReef.java
+++ src/main/java/frc/robot/subsystems/RobotAuto/MasterCommand/HomeToReef.java
@@ -1,10 +1,13 @@
 package frc.robot.subsystems.RobotAuto.MasterCommand;
 
+import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.DriveTrain.DriveBase;
 import frc.robot.subsystems.DriveTrain.DriveBaseConstants;
+import frc.robot.subsystems.RobotAuto.RobotAutoConstants;
 
 
 public class HomeToReef extends Command {
@@ -33,23 +36,47 @@ public class HomeToReef extends Command {
     }
 
     @Override
-    public void initialize() {
+    public void execute() {
+        Pose2d robotPose = driveBase.getPose();
 
+        double xOutput = XController.calculate(robotPose.getX(), targetPose.getX());
+        double yOutput = YController.calculate(robotPose.getY(), targetPose.getY());
+        double thetaOutput =
+                ThetaController.calculate(robotPose.getRotation().getRadians(), targetPose.getRotation().getRadians());
+
+        ChassisSpeeds fieldRelativeSpeeds = new ChassisSpeeds(xOutput, yOutput, thetaOutput);
+
+        ChassisSpeeds robotRelativeSpeeds =
+                ChassisSpeeds.fromFieldRelativeSpeeds(fieldRelativeSpeeds, driveBase.getRotation2d());
+
+        driveBase.drive(clampChassisSpeeds(robotRelativeSpeeds));
     }
 
-    @Override
-    public void execute() {
+    private static ChassisSpeeds clampChassisSpeeds(ChassisSpeeds robotRelativeSpeeds) {
+        double maxXYSpeed = RobotAutoConstants.HomingConstants.MAX_HOME_SPEED_METERS_PER_SECOND;
+        double maxThetaSpeed = RobotAutoConstants.HomingConstants.MAX_HOME_SPEED_RADIANS_PER_SECOND;
 
+        return new ChassisSpeeds(
+                MathUtil.clamp(robotRelativeSpeeds.vxMetersPerSecond, -maxXYSpeed, maxXYSpeed),
+                MathUtil.clamp(robotRelativeSpeeds.vyMetersPerSecond, -maxXYSpeed, maxXYSpeed),
+                MathUtil.clamp(robotRelativeSpeeds.omegaRadiansPerSecond, -maxThetaSpeed, maxThetaSpeed)
+        );
     }
 
     @Override
     public boolean isFinished() {
-        // TODO: Make this return true when this Command no longer needs to run execute()
-        return false;
+        double xError = XController.getError();
+        double yError = YController.getError();
+        double thetaError = ThetaController.getError();
+
+        double xyTolerance = RobotAutoConstants.HomingConstants.XY_TOLERANCE;
+        double thetaTolerance = RobotAutoConstants.HomingConstants.THETA_TOLERANCE;
+
+        return xError <= xyTolerance && yError <= xyTolerance && thetaError <= thetaTolerance;
     }
 
     @Override
     public void end(boolean interrupted) {
-
+        driveBase.drive(new ChassisSpeeds());
     }
 }
--- src/main/java/frc/robot/subsystems/RobotAuto/RobotAutoConstants.java
+++ src/main/java/frc/robot/subsystems/RobotAuto/RobotAutoConstants.java
@@ -12,6 +12,18 @@ public class RobotAutoConstants {
 
     public static ArrayList<Pose2d> reefPoses = new ArrayList<>();
 
+    public static class HomingConstants{
+        public static double MAX_HOME_SPEED_METERS_PER_SECOND = 0.5;
+        public static double MAX_HOME_SPEED_RADIANS_PER_SECOND = 0.5;
+
+        public static double XY_TOLERANCE = 0.1;
+        public static double THETA_TOLERANCE = 0.1;
+    }
+
+
+
+
+
     public static enum FeederLocations{
         TOP,
         BOTTOM;
