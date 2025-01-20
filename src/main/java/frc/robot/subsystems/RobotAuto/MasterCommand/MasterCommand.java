42fa030817f56e44f4e7fcaebda419469ec358e4
captainsoccer <justeyallevi@gmail.com>
2025-01-20 13:59:47 +0200

    made it more wrapper

--- src/main/java/frc/robot/subsystems/RobotAuto/MasterCommand/MasterCommand.java
+++ src/main/java/frc/robot/subsystems/RobotAuto/MasterCommand/MasterCommand.java
@@ -1,22 +1,117 @@
 package frc.robot.subsystems.RobotAuto.MasterCommand;
 
-
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.Subsystem;
+import frc.robot.Constants;
+import frc.robot.commands.Elevator.MoveToLevel;
+import frc.robot.commands.EndEffector.Eject;
 import frc.robot.subsystems.DriveTrain.DriveBase;
 import frc.robot.subsystems.Elevator.Elevator;
 import frc.robot.subsystems.Elevator.ElevatorConstants;
 import frc.robot.subsystems.EndEffector.EndEffector;
+import frc.robot.subsystems.EndEffector.EndEffectorConstants;
 
+import java.util.Set;
 import java.util.function.Supplier;
 
-public class MasterCommand extends SequentialCommandGroup {
+
+public class MasterCommand extends Command {
+
+    private final DriveBase driveBase;
+    private final Supplier<ElevatorConstants.ElevatorLevel> levelSupplier;
+    private final Supplier<Pose2d> targetPoseSupplier;
+
+    private Command coralCommand;
+
+    private Command pathCommand;
+    private final MoveToLevel moveElevatorCommand;
+    private final Eject ejectCommand;
+    private final Command elevatorToHome;
+    private final HomeToReef homeToReef;
+    private final Command adjustmentPhase;
+
+
     public MasterCommand(DriveBase driveBase, Elevator elevator, EndEffector endEffector,
-                         Supplier<ElevatorConstants.ElevatorLevel> levelSupplier, Supplier<Pose2d> targetPoseSupplier,
-                         Supplier<Pose2d> chosenFeederSupplier) {
-        addCommands(
-                new CoralCommand(driveBase, elevator, endEffector, levelSupplier, targetPoseSupplier).onlyIf(endEffector::gpLoaded),
-                new FeederCommand(driveBase, chosenFeederSupplier).onlyIf(() -> !endEffector.gpLoaded())
+                         Supplier<ElevatorConstants.ElevatorLevel> levelSupplier, Supplier<Pose2d> targetPoseSupplier) {
+        
+        this.driveBase = driveBase;
+        this.targetPoseSupplier = targetPoseSupplier;
+
+        moveElevatorCommand = new MoveToLevel(elevator, ElevatorConstants.ElevatorLevel.Intake);
+        ejectCommand = new Eject(endEffector, EndEffectorConstants.MotorPower.L1);
+
+        elevatorToHome = new MoveToLevel(elevator, ElevatorConstants.ElevatorLevel.Bottom);
+
+        homeToReef = new HomeToReef(driveBase, Constants.ReefLocation.REEF_1.getPose());
+
+        this.adjustmentPhase = new ParallelCommandGroup(
+                homeToReef,
+                moveElevatorCommand
         );
+
+        this.levelSupplier = levelSupplier;
+    }
+
+    public Command getPathCommand(){
+        return pathCommand;
+    }
+
+    @Override
+    public void initialize() {
+        Pose2d targetPose = targetPoseSupplier.get();
+        pathCommand = driveBase.findPath(targetPose);
+        homeToReef.setTargetPose(targetPose);
+
+        ElevatorConstants.ElevatorLevel level = levelSupplier.get();
+
+        moveElevatorCommand.setDesiredLevel(level);
+        ejectCommand.setMotorPower(getMotorPower(level));
+
+        coralCommand = new SequentialCommandGroup(
+                pathCommand,
+                adjustmentPhase,
+                ejectCommand,
+                elevatorToHome
+        );
+
+        coralCommand.initialize();
+    }
+
+    @Override
+    public void execute() {
+        coralCommand.execute();
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        coralCommand.end(interrupted);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return coralCommand.isFinished();
+    }
+
+    private EndEffectorConstants.MotorPower getMotorPower(ElevatorConstants.ElevatorLevel level){
+        return switch (level){
+            case L2,L3 -> EndEffectorConstants.MotorPower.L2_3;
+            case L4 -> EndEffectorConstants.MotorPower.L4;
+            default -> EndEffectorConstants.MotorPower.L1;
+        };
+    }
+
+    public Set<Subsystem> getRequirements() {
+        return this.coralCommand.getRequirements();
+    }
+
+    public boolean runsWhenDisabled() {
+        return this.coralCommand.runsWhenDisabled();
+    }
+
+    public Command.InterruptionBehavior getInterruptionBehavior() {
+        return this.coralCommand.getInterruptionBehavior();
     }
 }
