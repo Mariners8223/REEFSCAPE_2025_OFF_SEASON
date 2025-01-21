1512b109e21f041811029abf84f4492088bb6492
alontal2008 <132138466+alontal2008@users.noreply.github.com>
2025-01-21 18:03:20 +0200

    better enum values

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -6,6 +6,8 @@ package frc.robot;
 
 import java.util.Optional;
 
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.util.Units;
@@ -22,31 +24,54 @@ public class Constants {
         DEVELOPMENT,
         REPLAY
     }
-    public enum reef
-    {
-        REEF1 (5.864,3.8789,-180),
-        REEF2 (5.85,4.21,-180),
-        REEF3 (5.343,5.146,-120),
-        REEF4 (5.021,5.293,-120),
-        REEF5 (3.959,5.312,-60),
-        REEF6 (3.646,5.068,-60),
-        REEF7 (3.140,4.21,0),
-        REEF8 (3.13,3.849,0),
-        REEF9 (3.627,2.933,60),
-        REEF10 (3.91,2.796,60),
-        REEF11 (5.08,2.757,120),
-        REEF12 (5.304,2.913,120);
+    public enum ReefLocation {
+        REEF_1(3.173, 3.860, 0),
+        REEF_2(3.173, 4.190, 0),
+        REEF_3(3.686, 5.086, -60),
+        REEF_4(3.975, 5.246, -60),
+        REEF_5(5.053, 5.335, -120),
+        REEF_6(5.29, 5.08, -120),
+        REEF_7(5.803, 4.19, -180),
+        REEF_8(5.803, 3.86, -180),
+        REEF_9(5.289, 2.967, 120),
+        REEF_10(5.005, 2.807, 120),
+        REEF_11(3.973, 2.804, 60),
+        REEF_12(3.686, 2.97, 60);
 
-        private final Pose2d reefPose;
-        private reef(double x, double y, double degree){
-            reefPose = new Pose2d(
-                (DriverStation.getAlliance().get() == Alliance.Blue) ? x : 16-x, 
-                y, 
-                new Rotation2d(Units.radiansToDegrees(degree)));
+        private Pose2d pose;
+
+        public Pose2d getPose() {
+            return pose;
         }
 
-        public Pose2d getPose(){
-            return reefPose;
+        public boolean isBallInUpPosition(){
+            return switch (this){
+                case REEF_1, REEF_2, REEF_5, REEF_6, REEF_9, REEF_10 -> true;
+                case REEF_3, REEF_4, REEF_7, REEF_8, REEF_11, REEF_12 -> false;
+            };
+        }
+
+        public boolean isBallDropInSamePose(){
+            return (this.ordinal() + 1)%2 == 0;
+        }
+
+        public static void checkAlliance(boolean isBlue) {
+            if (isBlue) return;
+
+            AprilTagFields fields = AprilTagFields.k2025Reefscape;
+
+            AprilTagFieldLayout layout = AprilTagFieldLayout.loadField(fields);
+
+            for (ReefLocation r : ReefLocation.values()) {
+                r.pose = new Pose2d(
+                        layout.getFieldLength() - r.pose.getX(),
+                        layout.getFieldWidth() - r.pose.getY(),
+                        Rotation2d.fromDegrees(r.pose.getRotation().getDegrees() - 180));
+            }
+        }
+
+        ReefLocation(double x, double y, double deg) {
+            pose = new Pose2d(x, y, Rotation2d.fromDegrees(deg));
         }
     }
 
