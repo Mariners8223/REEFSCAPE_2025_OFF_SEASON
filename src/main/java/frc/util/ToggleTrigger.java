20b0bb62a4afd52599753d2aae4a37fe6ca0f7be
captainsoccer <justeyallevi@gmail.com>
2025-02-10 10:29:11 +0200

    fixed toggle trigger

--- src/main/java/frc/util/ToggleTrigger.java
+++ src/main/java/frc/util/ToggleTrigger.java
@@ -5,19 +5,13 @@ import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 public class ToggleTrigger {
-    private boolean lastState = false;
-
     public ToggleTrigger(Trigger trigger, Command command) {
 
-        Command fullCommand = command.andThen(new InstantCommand(() -> lastState = false));
-
         Runnable action = () -> {
-            if (lastState) {
-                fullCommand.cancel();
-                lastState = false;
+            if(command.isScheduled()){
+                command.cancel();
             } else {
-                fullCommand.schedule();
-                lastState = true;
+                command.schedule();
             }
         };
 
