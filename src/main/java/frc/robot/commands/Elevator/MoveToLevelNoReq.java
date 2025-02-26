5c38566d0616261cdb6ca0786d42d94204a8892b
captainsoccer <justeyallevi@gmail.com>
2025-02-26 12:45:05 +0200

    added move to level active

--- src/main/java/frc/robot/commands/Elevator/MoveToLevelNoReq.java
+++ src/main/java/frc/robot/commands/Elevator/MoveToLevelNoReq.java
@@ -43,6 +43,6 @@ public class MoveToLevelNoReq extends Command {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return elevator.getCurrentLevel() == desiredLevel;
+    return elevator.isAtDesiredLevel();
   }
 }
