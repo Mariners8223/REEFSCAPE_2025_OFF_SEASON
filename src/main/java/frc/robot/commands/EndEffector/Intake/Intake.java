fbbdd353a7a53abc650f3dd37e46656ff03afe71
unknown <ironih.frc@gmail.com>
2025-03-11 18:08:37 +0200

    Update Intake.java

--- src/main/java/frc/robot/commands/EndEffector/Intake/Intake.java
+++ src/main/java/frc/robot/commands/EndEffector/Intake/Intake.java
@@ -5,6 +5,7 @@
 package frc.robot.commands.EndEffector.Intake;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.subsystems.EndEffector.EndEffector;
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
@@ -13,7 +14,7 @@ import frc.robot.subsystems.EndEffector.EndEffector;
 public class Intake extends SequentialCommandGroup {
 
   public Intake(EndEffector endEffector) {
-    addCommands(new IntakeStep1(endEffector), new IntakeStep2(endEffector));
+    addCommands(new IntakeStep1(endEffector), new WaitCommand(0.25), new IntakeStep2(endEffector));
   }
   
 }
