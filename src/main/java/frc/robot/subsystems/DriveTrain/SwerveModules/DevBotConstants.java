1caec33a0cc5510147965b4c1d62d3574f88ba89
unknown <ironih.frc@gmail.com>
2025-03-14 15:37:06 +0200

    change to L2 and devBot

--- src/main/java/frc/robot/subsystems/DriveTrain/SwerveModules/DevBotConstants.java
+++ src/main/java/frc/robot/subsystems/DriveTrain/SwerveModules/DevBotConstants.java
@@ -31,7 +31,7 @@ public enum DevBotConstants {
             new PIDFGains(25, 20, 0, 0, 0.001, 0.05), //Steer motor PID
             0.16066, 1.6962, 0.62667, 0.43868, 1.8036, 0.35004);
 
-    public static final double DRIVE_GEAR_RATIO = 5.14;
+    public static final double DRIVE_GEAR_RATIO = 6.12;
     public static final double STEER_GEAR_RATIO = 12.8;
     public static final double WHEEL_RADIUS_METERS = 0.0508;
     public static final double WHEEL_CIRCUMFERENCE_METERS = 2 * Math.PI * WHEEL_RADIUS_METERS;
