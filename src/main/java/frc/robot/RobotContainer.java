cc114ad0353885825a66b674f2f9e8a1317bdb67
danielc <88316439+dandan59832@users.noreply.github.com>
2025-08-17 12:15:00 +0300

    Update RobotContainer.java

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -17,38 +17,7 @@ import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.*;
 import edu.wpi.first.wpilibj2.command.button.*;
-import frc.robot.Constants.FeederLocation;
-import frc.robot.Constants.FeederSide;
-import frc.robot.Constants.ReefLocation;
 import frc.robot.Constants.RobotType;
-import frc.robot.commands.BallDropping.BallDropOff;
-import frc.robot.commands.BallDropping.BallDropOnForHigh;
-import frc.robot.commands.BallDropping.BallDropOnForLow;
-import frc.robot.commands.BallDropping.Sequence.BallDropHigh;
-import frc.robot.commands.BallDropping.Sequence.BallDropLow;
-import frc.robot.commands.Climb.ClimbCommand;
-import frc.robot.commands.Drive.DriveCommand;
-import frc.robot.commands.Drive.MinorAdjust;
-import frc.robot.commands.Drive.MinorAdjust.AdjustmentDirection;
-import frc.robot.commands.Elevator.MoveToLevel;
-import frc.robot.commands.Elevator.MoveToLevelActive;
-import frc.robot.commands.EndEffector.EjectSequance;
-import frc.robot.commands.EndEffector.MiniEject;
-import frc.robot.commands.EndEffector.Funnel.ToggleFunnel;
-import frc.robot.commands.EndEffector.Intake.Intake;
-import frc.robot.commands.MasterCommand.*;
-import frc.robot.subsystems.BallDropping.BallDropping;
-import frc.robot.subsystems.Climb.Climb;
-import frc.robot.subsystems.Elevator.Elevator;
-import frc.robot.subsystems.Elevator.ElevatorConstants.ElevatorLevel;
-import frc.robot.subsystems.EndEffector.EndEffector;
-import frc.robot.subsystems.EndEffector.EndEffectorConstants.MotorPower;
-import frc.robot.subsystems.LED.LED;
-import frc.robot.subsystems.LED.LED.StripControl;
-import frc.robot.subsystems.RobotAuto.RobotAuto;
-
-import frc.robot.subsystems.Vision.Vision;
-import frc.util.Elastic;
 
 import org.json.simple.parser.ParseException;
 import org.littletonrobotics.conduit.ConduitApi;
@@ -66,13 +35,29 @@ import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.RobotState;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.util.Color;
-import frc.robot.subsystems.DriveTrain.DriveBase;
-
 
 public class RobotContainer {
+    public static MotorTesting motorTesting;
+
+    //public static CommandXboxController driveController;
+    public static CommandPS5Controller driveController;
 
     public RobotContainer() {
+        //driveController = new CommandXboxController(0);
+        driveController = new CommandPS5Controller(0);
 
-}
+        motorTesting = new MotorTesting();
+        driveController.cross().whileTrue(new TestMotorSpinCommand(motorTesting, -1));
+        driveController.triangle().whileTrue(new TestMotorSpinCommand(motorTesting, 1));
+
+    }
+
+//    configureDriveBindings();
+//
+//
+//    public void configureDriveBindings(){
+//        driveController.a().whileTrue(new TestMotorSpinCommand(motorTesting));
+//    }
+
 
 }
