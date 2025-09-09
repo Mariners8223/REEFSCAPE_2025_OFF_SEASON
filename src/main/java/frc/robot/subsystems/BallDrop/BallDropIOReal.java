10af3df2b70b5f6bec9d04cb2fa80b35f2924590
unknown <ironih.frc@gmail.com>
2025-09-09 17:48:03 +0300

    subsystem and IO

---
+++ src/main/java/frc/robot/subsystems/BallDrop/BallDropIOReal.java
@@ -0,0 +1,35 @@
+package frc.robot.subsystems.BallDrop;
+
+public class BallDropIOReal implements BallDropIO{
+
+    @Override
+    public void SetVoltageWheel(double voltage) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'SetVoltageWheel'");
+    }
+
+    @Override
+    public void resetMotorEncoder() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'resetMotorEncoder'");
+    }
+
+    @Override
+    public void SetVoltageArm(double voltage) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'SetVoltageArm'");
+    }
+
+    @Override
+    public void SetAngle(double angle) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'SetAngle'");
+    }
+
+    @Override
+    public void Update(BallDroppingInputs inputs) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'Update'");
+    }
+
+}
