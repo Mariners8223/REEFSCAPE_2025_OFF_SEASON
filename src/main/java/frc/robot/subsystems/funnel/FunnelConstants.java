c4e517ca2017379c20a079c658f3511fd735a418
Eyal_Birger <eyal.birger2010@gmail.com>
2025-09-12 16:17:42 +0300

    climb ioreal update

--- src/main/java/frc/robot/subsystems/funnel/FunnelConstants.java
+++ src/main/java/frc/robot/subsystems/funnel/FunnelConstants.java
@@ -1,5 +1,28 @@
-public class FunnelConstants {
-    public static final double FUNNEL_ANGLE_HIGH = 40;
-    public static final double START_ENDGAME = 30;
-    public static final double FUNNEL_ANGLE_LOW = 0;
+
+public package frc.robot.subsystems.funnel;
+
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
+import frc.util.MarinersController.MarinersController.ControllerLocation;
+
+class funnelConstants {
+    
+
+    public class FunnelConstants {
+        public static final double FUNNEL_ANGLE_HIGH = 40;
+        public static final double START_ENDGAME = 30;
+        public static final double FUNNEL_ANGLE_LOW = 0;
+    }
+    public static class funnelMotor {
+        public static final ControllerLocation CONTROLLER_LOCATION = ControllerLocation.MOTOR;
+        public static final int MOTOR_ID = 17;
+        public static final boolean IS_BRUSHLESS = true;
+        public static final MotorType MOTOR_TYPE = MotorType.Talon_FX;
+        public static final boolean IS_INVERTED = false;
+        public static final double ROTATION_TO_ANGLE = 8;
+        
+    }
+
+
+
 }
---
+++ src/main/java/frc/robot/subsystems/funnel/funnelIOReal.java
@@ -0,0 +1,23 @@
+package frc.robot.subsystems.funnel;
+
+import frc.robot.subsystems.funnel.funnelConstants.FunnelConstants;
+import frc.robot.subsystems.funnel.funnelConstants.funnelMotor;
+import frc.util.MarinersController.MarinersController;
+import frc.util.MarinersController.MarinersSparkBase;
+import frc.util.MarinersController.MarinersTalonFX;
+
+public class funnelIOReal implements FunnelIO {
+    private final MarinersTalonFX funnelMotor;
+    @SuppressWarnings ("unused")
+
+    public funnelIOReal(){
+        this.funnelMotor = configureLeadMotor();   
+    }
+
+    private  MarinersTalonFX configureLeadMotor(){
+        MarinersTalonFX motor = new MarinersTalonFX("Funnel Motor", funnelConstants.funnelMotor.CONTROLLER_LOCATION,
+            funnelConstants.funnelMotor.MOTOR_ID, funnelConstants.funnelMotor.ROTATION_TO_ANGLE);
+        return motor;
+        
+    }
+}
