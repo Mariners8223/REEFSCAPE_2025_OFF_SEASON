1fefe1ebec41d1e9e8e82f34d2583066759ff9d2
Eyal_Birger <eyal.birger2010@gmail.com>
2025-09-16 17:28:53 +0300

    robotContainer!

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -18,6 +18,7 @@ import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.*;
 import edu.wpi.first.wpilibj2.command.button.*;
 import frc.robot.Constants.RobotType;
+import frc.robot.commands.Drive.DriveCommand;
 
 import org.json.simple.parser.ParseException;
 import org.littletonrobotics.conduit.ConduitApi;
@@ -31,31 +32,44 @@ import com.pathplanner.lib.commands.PathPlannerAuto;
 import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.RobotState;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.util.Color;
-
+import frc.robot.commands.EndEffector.*;
 import frc.robot.subsystems.DriveTrain.DriveBase;
+import frc.robot.subsystems.EndEffector.EndEffector;
 
 public class RobotContainer {
     public static DriveBase driveBase;
-
+    public static DigitalInput beambreak;
+    public static EndEffector endEffector;
     public static CommandPS5Controller driveController;
+    public static CommandXboxController driveXboxController;
+
 
     public RobotContainer() {
-        driveController = new CommandPS5Controller(0);
-
+        driveController = new CommandPS5Controller(1);
+        driveXboxController = new CommandXboxController(0);
+        beambreak = new DigitalInput(41);
         driveBase = new DriveBase();
-
+        endEffector = new EndEffector();
         configureDriveBindings();
         
     }
 
 
-
-   public void configureDriveBindings(){
+    public void configureDriveBindings(){
+        new Trigger(RobotState::isTeleop).and(RobotState::isEnabled).whileTrue(new StartEndCommand(() ->
+            driveBase.setDefaultCommand(new DriveCommand(driveBase, driveXboxController)),
+            driveBase::removeDefaultCommand).ignoringDisable(true));
+        driveController.L1().onTrue(new CoralScoring(endEffector));
+        driveController.R1().onTrue(new scoringL234(endEffector));
+        driveController.touchpad().whileFalse(new collecting(endEffector));
+        
    }
+   
 
 
 }
