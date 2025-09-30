c8f92f1afde4ee619e5c9cf7c15b38384c12e1a1
Eyal_Birger <eyal.birger2010@gmail.com>
2025-09-30 15:40:26 +0300

    IOReal

--- src/main/java/frc/robot/subsystems/endEffector/EndEffectorConstents.java
+++ src/main/java/frc/robot/subsystems/endEffector/EndEffectorConstents.java
@@ -3,4 +3,7 @@ package frc.robot.subsystems.endEffector;
 public class EndEffectorConstents {
     public static final double DUTY_CYCLE = 0.8;
     public static boolean IsCoralInRobot = false;
+    public static final int R_ID = 22;
+    public static final int L_ID = 21;
+    public static final int BEAM_BREAK_PORT = 9;
 }
--- src/main/java/frc/robot/subsystems/endEffector/EndEffectorIOReal.java
+++ src/main/java/frc/robot/subsystems/endEffector/EndEffectorIOReal.java
@@ -1,5 +1,26 @@
 package frc.robot.subsystems.endEffector;
 
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import frc.robot.subsystems.endEffector.EndEffectorConstents;
+import edu.wpi.first.wpilibj.DigitalInput;
+
 public class EndEffectorIOReal implements EndEffectorIO{
+    private final VictorSPX RightMotor;
+    private final VictorSPX LeftMotor;
+    private final DigitalInput beambreak;
+
+    public EndEffectorIOReal(){
+      RightMotor = configureEndEffectorMotor(EndEffectorConstents.R_ID);
+      LeftMotor = configureEndEffectorMotor(EndEffectorConstents.L_ID);
+      beambreak = new DigitalInput(EndEffectorConstents.BEAM_BREAK_PORT);
+    }
+
+    private VictorSPX configureEndEffectorMotor(int ID) {
+        VictorSPX motor = new VictorSPX(ID);
+        motor.setNeutralMode(NeutralMode.Brake);
+        return motor;
+    }
+
 
 }
