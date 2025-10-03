1229bfbb4bf8e2abe68f25f33364f783e8dfa471
Eyal_Birger <eyal.birger2010@gmail.com>
2025-10-03 10:41:41 +0300

    zzzzzzzzzzzzzzzzzzzzzzzzzzzzzz

---
+++ src/main/java/frc/robot/subsystems/Climb/ClimbConstents.java
@@ -0,0 +1,22 @@
+
+public package frc.robot.subsystems.funnel;
+
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
+import frc.util.MarinersController.MarinersController.ControllerLocation;
+
+class funnelConstants {
+    
+
+    public class climbConstants {
+        public static final double CLIMB_MOTOR_HIGH; 
+        public static final double CLIMB_MOTOR_LOW;
+        public static final double
+    }
+    public static class climnMotor {
+        public static final ControllerLocation CONTROLLER_LOCATION = ControllerLocation.MOTOR;
+        public static final int MOTOR_ID = 18;
+        public static final MotorType MOTOR_TYPE = MotorType.Talon_FX;
+        
+    }
+}
