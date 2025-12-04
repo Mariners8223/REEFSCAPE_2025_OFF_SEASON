4259e0bf142999a75984878b72877073b6d49749
gali8223 <galibronrot135@gmail.com>
2025-12-04 20:27:21 +0200

    continued io

--- src/main/java/frc/robot/subsystems/ShooterGali/ShooterIO.java
+++ src/main/java/frc/robot/subsystems/ShooterGali/ShooterIO.java
@@ -9,9 +9,14 @@ public interface ShooterIO {
     @AutoLog
     class ShooterInputs{
         Pose3d shooter3DPose;
-        double angle;
     }
     double getPosition();
+
     void setVoltage(double voltage);
+
     void Update(ShooterInputs inputs);
+    
+    void setAngle(double angle);
+
+    void resetAngleEncoder();
 }
